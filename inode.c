jgowans/simplefs

commit e82c413b0c215f32a2b8a3497e1e62bf9585a1dc

Change parameter to initialize for Linux v6.3 (#28)
The parameter is changed to eliminate errors while initializing inode.

Reported by Cicak Bin Kadal.

inode.c
@@ -196,7 +196,7 @@ static struct inode *simplefs_new_inode(struct inode *dir, mode_t mode)
 
     if (S_ISLNK(mode)) {
 #if MNT_IDMAP_REQUIRED()
-        inode_init_owner(&nop_mnt_idmap, inode, dir, inode->mode);
+        inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
 #elif USER_NS_REQUIRED()
         inode_init_owner(&init_user_ns, inode, dir, mode);
 #else
@@ -219,7 +219,7 @@ static struct inode *simplefs_new_inode(struct inode *dir, mode_t mode)
 
     /* Initialize inode */
 #if MNT_IDMAP_REQUIRED()
-    inode_init_owner(&nop_mnt_idmap, inode, dir, inode->mode);
+    inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
 #elif USER_NS_REQUIRED()
     inode_init_owner(&init_user_ns, inode, dir, mode);
 #else

commit 6343fe70d2bc3a0629e3bd43d31be3f691ba7747

inode: Correct struct inode member to i_mode
When the update was made to use the mode field of the inode, it was
missed that the member is actually called i_mode. Fix that here.

Fixes: e82c413b0c21 ("Change parameter to initialize for Linux v6.3 (#28)")

Signed-off-by: James Gowans <gowans.james@gmail.com>

inode.c
@@ -196,7 +196,7 @@ static struct inode *simplefs_new_inode(struct inode *dir, mode_t mode)
 
     if (S_ISLNK(mode)) {
 #if MNT_IDMAP_REQUIRED()
-        inode_init_owner(&nop_mnt_idmap, inode, dir, inode->mode);
+        inode_init_owner(&nop_mnt_idmap, inode, dir, inode->i_mode);
 #elif USER_NS_REQUIRED()
         inode_init_owner(&init_user_ns, inode, dir, mode);
 #else
@@ -219,7 +219,7 @@ static struct inode *simplefs_new_inode(struct inode *dir, mode_t mode)
 
     /* Initialize inode */
 #if MNT_IDMAP_REQUIRED()
-    inode_init_owner(&nop_mnt_idmap, inode, dir, inode->mode);
+    inode_init_owner(&nop_mnt_idmap, inode, dir, inode->i_mode);
 #elif USER_NS_REQUIRED()
     inode_init_owner(&init_user_ns, inode, dir, mode);
 #else